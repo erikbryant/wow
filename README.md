d6b8d3d18921b7ecadd7711d3ac5d04c6d043150
Erik Bryant <erikbryantology@gmail.com>
2018-11-25 15:51:15 -0800

    Download the WoW auction house data and look through it for bargains.

--- README.md
+++ README.md
@@ -3,3 +3,57 @@ WoW API tools
 
 Tools that use the World of Warcraft developer API.
 https://develop.battle.net/
+
+SQL statements
+ # Create WoW environment
+ sudo mysql -u root -p
+ create database wow;
+ GRANT ALL PRIVILEGES ON wow.* TO 'wow'@'localhost' IDENTIFIED BY 'wowpassword';
+
+ CREATE TABLE items (
+    id int,
+    name varchar(128),
+    sellPrice int,
+    json varchar(5000)
+ );
+
+ CREATE UNIQUE INDEX id ON items ( id );
+
+ # Change root password
+ service mysql stop
+ mkdir -p /var/run/mysqld
+ chown mysql:mysql /var/run/mysqld
+ mysqld_safe --skip-grant-tables &
+ mysql
+ UPDATE mysql.user SET authentication_string=PASSWORD('new_password') WHERE User='root';
+ FLUSH PRIVILEGES;
+ (kill any mysql procs running)
+ service mysql stop
+ service mysql start
+ sudo mysql -u root -p
+
+Backup / Restore
+
+mysqldump -u wow -p db_name t1 > dump.sql
+mysql -u wow -p db_name < dump.sql
+
+
+// Auction Keys:
+// rand
+// petSpeciesId
+// owner
+// buyout
+// context
+// bid
+// timeLeft
+// ownerRealm
+// petQualityId
+// modifiers
+// petBreedId
+// petLevel
+// quantity
+// item
+// seed
+// bonusLists
+// auc
+
--- wow.go
+++ wow.go
@@ -1,55 +1,173 @@
 package main
 
+// https://develop.battle.net/documentation
+// $ go get github.com/go-sql-driver/mysql
+
 import (
+	"./database"
 	"./web"
 	"flag"
 	"fmt"
+	"time"
 )
 
 var (
 	clientID     = flag.String("clientID", "", "WoW API client ID")
 	clientSecret = flag.String("clientSecret", "", "WoW API client secret")
+	realm        = flag.String("realm", "icecrown", "WoW realm")
 )
 
-// https://develop.battle.net/documentation
-
+// accessToken() retrieves an access token from battle.net. This token is used to authenticate API calls.
 func accessToken(id, secret string) string {
 	url := "https://us.battle.net/oauth/token?client_id=" + id + "&client_secret=" + secret + "&grant_type=client_credentials"
-	response := web.WebRequestJSON(url)
+	response := web.RequestJSON(url)
 	return response["access_token"].(string)
 }
 
-func auctionHouse(realm, accessToken string) []interface{} {
-	// Look up the URL for the latest auction house data.
+// getAuctionURL() retrieves the URL for the latest auction house data.
+func getAuctionURL(realm, accessToken string) string {
 	url := "https://us.api.blizzard.com/wow/auction/data/" + realm + "?locale=en_US&access_token=" + accessToken
-	response := web.WebRequestJSON(url)
+	response := web.RequestJSON(url)
 	data := response["files"].([]interface{})[0].(map[string]interface{})
-	auctionURL := data["url"].(string)
+	return data["url"].(string)
+}
 
-	// Retrieve the latest auction house data.
-	response = web.WebRequestJSON(auctionURL)
-	// realms := response["realms"].([]interface{})
+// getAuctions() retrieves the latest auctions from the auction house.
+func getAuctions(auctionURL string) []interface{} {
+	response := web.RequestJSON(auctionURL)
 	auctions := response["auctions"].([]interface{})
 	return auctions
 }
 
-func item(id, accessToken string) map[string]interface{} {
+// letsGoShopping() alerts if it finds bargain prices on goods I need.
+func letsGoShopping(auctions []interface{}) {
+	var goods = []struct {
+		item     int64
+		maxPrice int64
+		name     string
+	}{
+		{33447, 28000, "Runic Healing Potion"},
+		{34721, 28000, "Frostweave Bandage"},
+		{34722, 40000, "Heavy Frostweave Bandage"},
+
+		// Enchanting
+		{34054, 20000, "Infinite Dust"},
+		{34056, 25000, "Lesser Cosmic Essence"},
+	}
+
+	for _, a := range auctions {
+		auction := a.(map[string]interface{})
+		id := web.ToInt64(auction["item"])
+		quantity := web.ToInt64(auction["quantity"])
+		buyout := web.ToInt64(auction["buyout"])
+		unitBuyout := buyout / quantity
+
+		for _, good := range goods {
+			if id == good.item && unitBuyout < good.maxPrice {
+				discount := good.maxPrice - unitBuyout
+				fmt.Printf("Shop  '%s' %s %d %d\n", good.name, auction["owner"], quantity, discount)
+			}
+		}
+	}
+}
+
+func webGetItem(id, accessToken string) map[string]interface{} {
 	url := "https://us.api.blizzard.com/wow/item/" + id + "?locale=en_US&access_token=" + accessToken
-	return web.WebRequestJSON(url)
+	return web.RequestJSON(url)
+}
+
+func lookupItem(id int64, accessToken string) database.Item {
+	var item database.Item
+	var ok bool
+
+	// Do we have it cached in the database?
+	item, ok = database.LookupItem(id)
+	if ok {
+		return item
+	}
+
+	i := webGetItem(web.ToString(id), accessToken)
+	_, ok = i["sellPrice"]
+	if !ok {
+		fmt.Println("Item had no sellPrice")
+		return item
+	}
+	item.Id = web.ToInt64(i["id"])
+	item.Name = i["name"].(string)
+	item.SellPrice = web.ToInt64(i["sellPrice"])
+	item.JSON = fmt.Sprintf("%v", i)
+
+	database.SaveItem(item)
+
+	return item
+}
+
+// arbitrage() alerts if it finds auction prices that are lower than vendor prices.
+func arbitrage(auctions []interface{}, accessToken string) {
+	blacklist := map[int64]bool{
+		71359:  true, // Chelley's Sterilized Scalpel
+		71366:  true, // Lava Bolt Crossbow
+		111475: true, // Beater's Beat Stick
+	}
+
+	for _, a := range auctions {
+		auction := a.(map[string]interface{})
+		id := web.ToInt64(auction["item"])
+		if blacklist[id] {
+			continue
+		}
+		item := lookupItem(id, accessToken)
+		sellPrice := item.SellPrice
+		bid := web.ToInt64(auction["bid"])
+		quantity := web.ToInt64(auction["quantity"])
+		buyout := web.ToInt64(auction["buyout"])
+		unitBid := bid / quantity
+		unitBuyout := buyout / quantity
+		if unitBuyout > 0 && unitBuyout < sellPrice {
+			profit := sellPrice*quantity - buyout
+			if profit < 100 {
+				continue
+			}
+			fmt.Printf("Buy!  '%s' %s %d %d\n", item.Name, auction["owner"], quantity, profit)
+		} else {
+			if unitBid < sellPrice {
+				profit := sellPrice*quantity - bid
+				if profit < 500 {
+					continue
+				}
+				fmt.Printf("Bid   '%s' %s %d %d\n", item.Name, auction["owner"], quantity, profit)
+			}
+		}
+	}
 }
 
 func main() {
 	flag.Parse()
 
-	realm := "icecrown"
-	token := accessToken(*clientID, *clientSecret)
+	database.Open()
+	defer database.Close()
 
-	auctions := auctionHouse(realm, token)
-	fmt.Println(auctions[0])
-	fmt.Println()
+	lastAuctionURL := ""
+	retries := 0
+	for {
+		token := accessToken(*clientID, *clientSecret)
 
-	first := auctions[0].(map[string]interface{})
-	i := item(web.ToString(first["item"]), token)
-	fmt.Println(i)
-	fmt.Println()
+		auctionURL := getAuctionURL(*realm, token)
+		if auctionURL == lastAuctionURL {
+			fmt.Println("No new AH data ...")
+			time.Sleep(60 * time.Second)
+			retries++
+			continue
+		}
+		fmt.Println("retries:", retries)
+		retries = 0
+		lastAuctionURL = auctionURL
+
+		auctions := getAuctions(auctionURL)
+
+		fmt.Println()
+		letsGoShopping(auctions)
+		fmt.Println()
+		arbitrage(auctions, token)
+	}
 }
