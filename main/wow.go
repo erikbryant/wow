b83a2fb010b9e837fc378f8164ad0a86861557bd
Erik Bryant <erikbryantology@gmail.com>
2024-06-17 11:38:32 -0400

    Update item prices, tidy code

--- main/wow.go
+++ main/wow.go
@@ -35,10 +35,10 @@ var (
 		7909:   5000,    // Aquamarine
 		22445:  12100,   // Arcane Dust
 		124440: 29800,   // Arkhana
-		124442: 225000,  // Chaos Crystal
+		124442: 190000,  // Chaos Crystal
 		109693: 6600,    // Draenic Dust
-		3819:   97500,   // Dragon's Teeth
-		9224:   730000,  // Elixir of Demonslaying
+		3819:   97300,   // Dragon's Teeth
+		9224:   720000,  // Elixir of Demonslaying
 		7082:   200000,  // Essence of Air
 		7076:   2000,    // Essence of Earth
 		7078:   5000,    // Essence of Fire
@@ -46,26 +46,26 @@ var (
 		7080:   458700,  // Essence of Water
 		23427:  310000,  // Eternium Ore
 		22794:  57500,   // Fel Lotus
-		124116: 777500,  // Felhide
-		124106: 1000000, // Felwort
+		124116: 742400,  // Felhide
+		124106: 999800,  // Felwort
 		4625:   55000,   // Firebloom
 		34056:  2400,    // Lesser Cosmic Essence
 		16202:  39200,   // Lesser Eternal Essence
 		10938:  4500,    // Lesser Magic Essence (720 in shop)
 		22447:  5000,    // Lesser Planar Essence
-		124441: 89800,   // Leylight Shard
+		124441: 72000,   // Leylight Shard
 		16204:  600,     // Light Illusion Dust
 		12803:  233900,  // Living Essence
-		111245: 75700,   // Luminous Shard
+		111245: 40400,   // Luminous Shard
 		52722:  10100,   // Maelstrom Crystal
-		22791:  80000,   // Netherbloom
-		22792:  52500,   // Nightmare Vine
-		22451:  4009900, // Primal Air
+		22791:  75000,   // Netherbloom
+		22792:  39900,   // Nightmare Vine
+		22451:  3000100, // Primal Air
 		22452:  74500,   // Primal Earth
 		21884:  1745000, // Primal Fire
-		21886:  958700,  // Primal Life
+		21886:  958600,  // Primal Life
 		22457:  1480000, // Primal Mana
-		23571:  7230000, // Primal Might
+		23571:  7220000, // Primal Might
 		22456:  1059800, // Primal Shadow
 		21885:  1064000, // Primal Water
 		156930: 3500,    // Rich Illusion Dust
@@ -73,7 +73,7 @@ var (
 		22448:  4500,    // Small Prismatic Shard
 		10940:  1000,    // Strange Dust (720 in shop)
 		113588: 72000,   // Temporal Crystal
-		22450:  11100,   // Void Crystal
+		22450:  9700,    // Void Crystal
 		52328:  159900,  // Volatile Air
 		8153:   3500000, // WildVine
 
@@ -187,10 +187,6 @@ func findArbitrages(auctions map[int64][]common.Auction, accessToken string) []B
 			continue
 		}
 		for _, auction := range aucs {
-			if item.Equippable {
-				// I do not understand how to price these
-				continue
-			}
 			if auction.Buyout < item.SellPrice {
 				bargain := Bargain{
 					Quantity:    auction.Quantity,
@@ -254,15 +250,6 @@ func main() {
 		log.Fatal("ERROR: Unable to obtain access token.")
 	}
 
-	//a, ok := wowAPI.Auctions(*realm, accessToken)
-	//if !ok {
-	//	log.Fatal("ERROR: Unable to obtain auctions.")
-	//}
-	//fmt.Printf("#Auctions: %d\n\n", len(a))
-	//auctions := unpackAuctions(a)
-	//toBuy = findBargains(usefulGoods, auctions, accessToken)
-	//printShoppingList(toBuy)
-
 	c, ok := wowAPI.Commodities(accessToken)
 	if !ok {
 		log.Fatal("ERROR: Unable to obtain commodity auctions.")
@@ -270,9 +257,28 @@ func main() {
 	fmt.Printf("#Commodities: %d\n\n", len(c))
 	commodities := unpackAuctions(c)
 
-	// Look for things to buy
+	// Look for commodities to buy
 	toBuy := findBargains(usefulGoods, commodities, accessToken)
 	printShoppingList("Bargains", toBuy)
 	toBuy = findArbitrages(commodities, accessToken)
 	printShoppingList("Arbitrages", toBuy)
+
+	// Non-commodity auctions are strange. They have items for sale.
+	// These items have sell prices. But, the sell prices in the
+	// item's data are not the sell prices that the vendors offer.
+	// How do we know what the actual vendor offer will be?
+	//
+	// Is this a reputation issue? Do we scale the sell price down
+	// based on how little reputation the seller has with this faction?
+
+	//a, ok := wowAPI.Auctions(*realm, accessToken)
+	//if !ok {
+	//	log.Fatal("ERROR: Unable to obtain auctions.")
+	//}
+	//fmt.Printf("#Auctions: %d\n\n", len(a))
+	//auctions := unpackAuctions(a)
+	//toBuy = findBargains(usefulGoods, auctions, accessToken)
+	//printShoppingList("Auctions", toBuy)
+	//toBuy = findArbitrages(auctions, accessToken)
+	//printShoppingList("Auctions", toBuy)
 }
