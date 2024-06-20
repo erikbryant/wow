8fdb7e97980ff1aae35fdfa615d8d9efc1cce486
Erik Bryant <erikbryantology@gmail.com>
2024-06-20 12:15:06 -0400

    Allow for lookup by itemId

--- listItems/listItems.go
+++ listItems/listItems.go
@@ -1,7 +1,40 @@
 package main
 
-import "github.com/erikbryant/wow/cache"
+import (
+	"flag"
+	"fmt"
+	"github.com/erikbryant/wow/cache"
+	"github.com/erikbryant/wow/wowAPI"
+	"log"
+)
+
+var (
+	passPhrase = flag.String("passPhrase", "", "Passphrase to unlock WOW API client Id/secret")
+	itemId     = flag.Int64("id", 0, "Item ID to look up")
+)
+
+// usage prints a usage message and terminates the program with an error
+func usage() {
+	log.Fatal("Usage: wow -passPhrase <phrase>")
+}
 
 func main() {
-	cache.Print()
+	flag.Parse()
+
+	if *itemId == 0 {
+		cache.Print()
+		return
+	}
+
+	if *passPhrase == "" {
+		fmt.Println("ERROR: You must specify -passPhrase to unlock the client Id/secret")
+		usage()
+	}
+
+	accessToken, ok := wowAPI.AccessToken(*passPhrase)
+	if !ok {
+		log.Fatal("ERROR: Unable to obtain access token.")
+	}
+
+	wowAPI.LookupItem(*itemId, accessToken)
 }
