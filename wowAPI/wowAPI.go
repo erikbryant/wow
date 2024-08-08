54e2b1a61984168878473c20252e539d3e0ea6d2
Erik Bryant <erikbryantology@gmail.com>
2024-08-08 17:09:41 -0400

    Remove unused code

--- wowAPI/wowAPI.go
+++ wowAPI/wowAPI.go
@@ -69,29 +69,6 @@ func realmToSlug(realm string) string {
 	return slug
 }
 
-// CoinsToString returns a human-readable, formatted version of the coin amount
-func CoinsToString(amount int64) string {
-	sign := ""
-	if amount < 0 {
-		sign = "-"
-		amount *= -1
-	}
-
-	copper := amount % 100
-	silver := (amount / 100) % 100
-	gold := amount / 10000
-
-	if gold > 0 {
-		return fmt.Sprintf("%s%d.%02d.%02d", sign, gold, silver, copper)
-	}
-
-	if silver > 0 {
-		return fmt.Sprintf("%s%d.%02d", sign, silver, copper)
-	}
-
-	return fmt.Sprintf("%s%d", sign, copper)
-}
-
 // AccessToken retrieves an access token from battle.net. This token is used to authenticate API calls.
 func AccessToken(passPhrase string) (string, bool) {
 	clientID, err := aes.Decrypt(clientIDCrypt, passPhrase)
