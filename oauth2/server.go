ded6c692fda4814410b030cadd043b7d6c71a0bb
Erik Bryant <erikbryantology@gmail.com>
2024-08-21 19:34:04 -0400

    Cleaner code

--- oauth2/server.go
+++ oauth2/server.go
@@ -5,6 +5,7 @@ import (
 	"crypto/rand"
 	"encoding/base64"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"golang.org/x/oauth2"
 	"golang.org/x/oauth2/endpoints"
@@ -17,16 +18,24 @@ import (
 )
 
 var (
+	// blizzardOauthConfig stores the OAUTH config for authenticating with Blizzard
 	blizzardOauthConfig = &oauth2.Config{
-		RedirectURL:  "http://localhost:8888/auth/blizzard/profile",
-		ClientID:     "",
-		ClientSecret: "",
-		Scopes:       []string{"wow.profile", "sc2.profile"},
+		ClientID:     "", // Populated at runtime
+		ClientSecret: "", // Populated at runtime
 		Endpoint:     endpoints.Battlenet,
+		RedirectURL:  "http://localhost:8888/auth/blizzard/profile",
+		Scopes:       []string{"wow.profile", "sc2.profile"},
 	}
+	// server is a reference to the webserver
 	server = &http.Server{}
 )
 
+const (
+	// cookieName is the name of the OAUTH cookie
+	cookieName = "oauthState"
+)
+
+// generateStateOauthCookie stores a unique identifier in a cookie and returns that same identifier
 func generateStateOauthCookie(w http.ResponseWriter) string {
 	var expiration = time.Now().Add(20 * time.Minute)
 
@@ -36,12 +45,13 @@ func generateStateOauthCookie(w http.ResponseWriter) string {
 		log.Fatal(err)
 	}
 	state := base64.URLEncoding.EncodeToString(b)
-	cookie := http.Cookie{Name: "oauthstate", Value: state, Expires: expiration}
+	cookie := http.Cookie{Name: cookieName, Value: state, Expires: expiration}
 	http.SetCookie(w, &cookie)
 
 	return state
 }
 
+// oauthBlizzardLogin creates the auth cookie and redirects to the Blizzard auth server
 func oauthBlizzardLogin(w http.ResponseWriter, r *http.Request) {
 	// Create oauthState cookie
 	oauthState := generateStateOauthCookie(w)
@@ -53,27 +63,12 @@ func oauthBlizzardLogin(w http.ResponseWriter, r *http.Request) {
 	http.Redirect(w, r, u, http.StatusTemporaryRedirect)
 }
 
-func oauthBlizzardCallback(w http.ResponseWriter, r *http.Request) {
-	// Read oauthState from Cookie
-	oauthState, _ := r.Cookie("oauthstate")
-
-	if r == nil {
-		log.Println("oauthBlizzardCallback: Empty request")
-		http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
-		return
-	}
-
-	if r.FormValue("state") != oauthState.Value {
-		log.Println("invalid oauth state")
-		http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
-		return
-	}
-
+func tokenToPAT(code string) string {
 	client := &http.Client{}
 	data := url.Values{
 		"redirect_uri": {blizzardOauthConfig.RedirectURL},
 		"grant_type":   {"authorization_code"},
-		"code":         {r.FormValue("code")},
+		"code":         {code},
 	}
 	request, err := http.NewRequest("POST", "https://oauth.battle.net/token", strings.NewReader(data.Encode()))
 	if err != nil {
@@ -98,11 +93,33 @@ func oauthBlizzardCallback(w http.ResponseWriter, r *http.Request) {
 		log.Fatal(err)
 	}
 
-	profileAccessToken := jsonObject["access_token"].(string)
-	w.Write([]byte("success!\n"))
-	w.Write([]byte(profileAccessToken))
+	return jsonObject["access_token"].(string)
 }
 
+// oauthBlizzardCallback receives the token, converts it to a PAT, and passes that to the webpage requester
+func oauthBlizzardCallback(w http.ResponseWriter, r *http.Request) {
+	// Read oauthState from Cookie
+	oauthState, _ := r.Cookie(cookieName)
+
+	if r == nil {
+		log.Println("oauthBlizzardCallback: Empty request")
+		http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
+		return
+	}
+
+	if r.FormValue("state") != oauthState.Value {
+		log.Println("invalid oauth state")
+		http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
+		return
+	}
+
+	// Exchange the token we got for an actual profile access token
+	pat := tokenToPAT(r.FormValue("code"))
+	w.Write([]byte("success!\n"))
+	w.Write([]byte(pat))
+}
+
+// handlers registers the OAUTH endpoints
 func handlers() http.Handler {
 	mux := http.NewServeMux()
 	// Root
@@ -115,6 +132,7 @@ func handlers() http.Handler {
 	return mux
 }
 
+// Start starts the webserver
 func Start(clientID, clientSecret string) {
 	blizzardOauthConfig.ClientID = clientID
 	blizzardOauthConfig.ClientSecret = clientSecret
@@ -126,9 +144,12 @@ func Start(clientID, clientSecret string) {
 
 	log.Printf("Starting HTTP Server. Listening at %v", server.Addr)
 	err := server.ListenAndServe()
-	log.Printf("%v", err)
+	if !errors.Is(err, http.ErrServerClosed) {
+		log.Printf("%v", err)
+	}
 }
 
+// Shutdown terminates the webserver
 func Shutdown() {
 	err := server.Shutdown(context.Background())
 	if err != nil {
