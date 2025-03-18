80876c5be2cdb05b3e2ce37d75d32c2742d1ac49
Erik Bryant <erikbryantology@gmail.com>
2025-03-18 13:44:52 -0700

    Stop ignoring some pets

--- main/wow.go
+++ main/wow.go
@@ -16,11 +16,12 @@ import (
 
 var (
 	passPhrase = flag.String("passPhrase", "", "Passphrase to unlock WOW API client Id/secret")
-	realms     = flag.String("realms", "Commodities,Aegwynn,Agamaggan,Akama,Alexstrasza,Altar of Storms,Andorhal,Anub'arak,Argent Dawn,Azgalor,Azjol-Nerub,Azuremyst,Baelgun,Blackhand,Blackwing Lair,Bloodhoof,Bronzebeard,Cairne,Coilfang,Deathwing,Dentarg,Draenor,Drak'thul,Durotan,Eitrigg,Elune,Farstriders,Feathermoon,Frostwolf,Ghostlands,Greymane,IceCrown,Kilrogg,Kul Tiras,Llane,Misha,Ravencrest,Sisters of Elune", "WoW realms")
+	realms     = flag.String("realms", "Aegwynn,Agamaggan,Akama,Alexstrasza,Altar of Storms,Andorhal,Anub'arak,Argent Dawn,Azgalor,Azjol-Nerub,Azuremyst,Baelgun,Blackhand,Blackwing Lair,Bloodhoof,Bronzebeard,Cairne,Coilfang,Deathwing,Dentarg,Draenor,Drak'thul,Durotan,Eitrigg,Elune,Farstriders,Feathermoon,Frostwolf,Ghostlands,Greymane,IceCrown,Kilrogg,Kul Tiras,Llane,Misha,Ravencrest,Sisters of Elune", "WoW realms")
 	realmsUS   = flag.Bool("realmsUS", false, "Scan all other US realms")
 
 	// restOfUS is the rest of the realms in the US
 	restOfUS = []string{
+		//"Commodities",
 		"Aggramar",
 		"Alleria",
 		"Alterac Mountains",
@@ -147,15 +148,15 @@ func printPetBargains(auctions map[int64][]auction.Auction) {
 
 	// SpeciesId of pets that do not resell well
 	skipPets := map[int64]bool{
-		117:  true, // Tiny Snowman
-		119:  true, // Father Winter's Helper
-		120:  true, // Winter's Little Helper
-		153:  true, // Wolpertinger
-		162:  true, // Sinister Squashling
-		211:  true, // Strand Crawler
-		1662: true, // Cinder Pup
-		1687: true, // Left Shark
-		1706: true, // Ashmaw Cub
+		//117:  true, // Tiny Snowman
+		//119:  true, // Father Winter's Helper
+		//120:  true, // Winter's Little Helper
+		//153:  true, // Wolpertinger
+		//162:  true, // Sinister Squashling
+		//211:  true, // Strand Crawler
+		//1662: true, // Cinder Pup
+		//1687: true, // Left Shark
+		//1706: true, // Ashmaw Cub
 
 	}
 	for _, petAuction := range auctions[battlePet.PetCageItemId] {
