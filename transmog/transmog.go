7f9aa47e568942c91faadcb68ba515e66bebe9d4
Erik Bryant <erikbryantology@gmail.com>
2025-08-05 13:06:30 +0200

    Remove items from the skip list

--- transmog/transmog.go
+++ transmog/transmog.go
@@ -139,27 +139,20 @@ func NeedId(id int64) bool {
 	return !allOwned[id]
 }
 
-// flaky contains item IDs for which WoW says I own the transmogs, but this app says I don't
+// flaky item IDs; WoW says I own the transmogs, but this app says I don't
 var flaky = map[int64]bool{
-	cache.Search("15 Pound Salmon").Id():               true,
-	cache.Search("17 Pound Catfish").Id():              true,
-	cache.Search("18 Pound Salmon").Id():               true,
-	cache.Search("19 Pound Catfish").Id():              true,
-	cache.Search("22 Pound Catfish").Id():              true,
-	cache.Search("Anthemic Bracers").Id():              true,
-	cache.Search("Anthemic Cuirass").Id():              true,
-	cache.Search("Anthemic Gauntlets").Id():            true,
-	cache.Search("Arclight Spanner").Id():              true,
-	cache.Search("Blacksmith Hammer").Id():             true,
-	cache.Search("Choral Amice").Id():                  true,
-	cache.Search("Choral Sash").Id():                   true,
-	cache.Search("Choral Slippers").Id():               true,
-	cache.Search("Exploding Shot").Id():                true,
-	cache.Search("Light Shot").Id():                    true,
-	cache.Search("Shatter Rounds").Id():                true,
-	cache.Search("Solid Shot").Id():                    true,
-	cache.Search("Staccato Belt").Id():                 true,
-	cache.Search("Waistclasp of Unethical Power").Id(): true,
+	cache.Search("15 Pound Salmon").Id():   true,
+	cache.Search("17 Pound Catfish").Id():  true,
+	cache.Search("18 Pound Salmon").Id():   true,
+	cache.Search("19 Pound Catfish").Id():  true,
+	cache.Search("22 Pound Catfish").Id():  true,
+	cache.Search("Arclight Spanner").Id():  true,
+	cache.Search("Blacksmith Hammer").Id(): true,
+	cache.Search("Exploding Shot").Id():    true,
+	cache.Search("Heavy Shot").Id():        true,
+	cache.Search("Light Shot").Id():        true,
+	cache.Search("Shatter Rounds").Id():    true,
+	cache.Search("Solid Shot").Id():        true,
 }
 
 // NeedItem returns true if I need any of the transmogs this item provides
