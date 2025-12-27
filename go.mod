452a83d64868f0fc13007e4364d127934a923c34
Erik Bryant <erikbryantology@gmail.com>
2025-12-27 10:52:53 -0800

    Update skip lists

--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
 module github.com/erikbryant/wow
 
-go 1.25.3
+go 1.25.5
 
 require (
 	github.com/erikbryant/aes v0.4.0
--- wowAPI/wowAPI.go
+++ wowAPI/wowAPI.go
@@ -27,6 +27,7 @@ var (
 
 	skipItems = map[int64]bool{
 		// Items not found in the WoW database
+		23958:  true,
 		23972:  true,
 		29557:  true,
 		29566:  true,
