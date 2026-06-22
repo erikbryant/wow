207e615647ebe533e33a0b65617fff9b7422b583
Erik Bryant <erikbryantology@gmail.com>
2026-06-21 21:20:09 -0700

    Add rest of Rr* alts

--- recipes/recipes.go
+++ recipes/recipes.go
@@ -105,6 +105,19 @@ var Alts = []Alt{
 	{"Farstriders", "Rrhooshka"},
 	{"Feathermoon", "Rrhannet"},
 	{"Frostwolf", "Rrouscha"},
+	//{"Ghostlands", "Rreezy"},
+	//{"Greymane", "Rrznyth"},
+	//{"Icecrown", "Pkhats"},
+	//{"Kilrogg", "Rrinky"},
+	//{"Kirin Tor", "Rruggles"},
+	//{"Kul Tiras", "Rrkulth"},
+	//{"Lightninghoof", "Rroovetta"},
+	//{"Llane", "Rreebenna"},
+	//{"Misha", "Rrazhneth"},
+	//{"Nazgrel", "Rryxny"},
+	//{"Ravencrest", "Rrothinna"},
+	//{"Runetotem", "Rrygellna"},
+	//{"Sisters of Elune", "Rrhette"},
 }
 
 func key(alt Alt) string {
