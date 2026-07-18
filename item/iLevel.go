9080a936d1b1f8a98f8c7d7f31c3e081f48d90ad
Erik Bryant <erikbryantology@gmail.com>
2026-07-18 11:09:30 -0700

    Add another item

--- item/iLevel.go
+++ item/iLevel.go
@@ -77,6 +77,7 @@ var itemLevels = map[int64][]int64{
 	222480: {79, 85, 91, 98, 105}, // Proficient Sickle
 
 	// iLvl 486 oddities
+	222846: {80, 81, 82, 83},          // Weavercloth Chef's Hat
 	244620: {180, 186, 192, 199, 206}, // Chemist's Cap
 	244707: {180, 186, 192, 199, 206}, // Farstrider Fabric Cutters
 	244709: {180, 186, 192, 199, 206}, // Junker's Junk Visor
