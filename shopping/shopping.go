63dfc16067a9ccf3ea3a46473d9e26a7e7bc19da
Erik Bryant <erikbryantology@gmail.com>
2026-07-24 16:48:35 -0700

    Revert to usual pet skip list

--- shopping/shopping.go
+++ shopping/shopping.go
@@ -140,43 +140,39 @@ func findPetBargains(auctions map[int64][]auction.Auction) []string {
 
 	// SpeciesId of pets that do not resell well
 	skipPets := map[int64]struct{}{
-		//1385: {}, // Albino Chimaeraling
-		//1706: {}, // Ashmaw Cub
-		//1150: {}, // Ashstone Core
-		//1934: {}, // Benax
-		//1964: {}, // Blood Boil
-		//4489: {}, // Bouncer
-		//4537: {}, // Chester
-		//1662: {}, // Cinder Pup
-		//2087: {}, // Cinderweb Recluse
-		//1149: {}, // Corefire Imp
-		//1205: {}, // Direhorn Runt
-		//119:  {}, // Father Winter's Helper
-		//1545: {}, // Firewing
-		//1442: {}, // Ghastly Kid
-		//1147: {}, // Harbinger of Flame
-		//2916: {}, // Hungry Burrower
-		//2089: {}, // Infernal Pyreclaw
-		//1687: {}, // Left Shark
-		//4647: {}, // Mr. DELVER
-		//1568: {}, // Puddle Terror
-		//340:  {}, // Sea Pony
-		//162:  {}, // Sinister Squashling
-		//1628: {}, // Sister of Temptation
-		//200:  {}, // Spring Rabbit
-		//211:  {}, // Strand Crawler
-		//2088: {}, // Surger
-		//1434: {}, // Sun Sproutling
-		//1570: {}, // Sunfire Kaliri
-		//117:  {}, // Tiny Snowman
-		//251:  {}, // Toxic Wasteling
-		//118:  {}, // Winter Reindeer
-		//120:  {}, // Winter's Little Helper
-		//153:  {}, // Wolpertinger
-
-		// Pets to skip when selling to Stephen
+		1385: {}, // Albino Chimaeraling
+		1706: {}, // Ashmaw Cub
+		1150: {}, // Ashstone Core
+		1934: {}, // Benax
+		1964: {}, // Blood Boil
+		4489: {}, // Bouncer
+		4537: {}, // Chester
 		1662: {}, // Cinder Pup
-		1907: {}, // Pygmy Owl
+		2087: {}, // Cinderweb Recluse
+		1149: {}, // Corefire Imp
+		1205: {}, // Direhorn Runt
+		119:  {}, // Father Winter's Helper
+		1545: {}, // Firewing
+		1442: {}, // Ghastly Kid
+		1147: {}, // Harbinger of Flame
+		2916: {}, // Hungry Burrower
+		2089: {}, // Infernal Pyreclaw
+		1687: {}, // Left Shark
+		4647: {}, // Mr. DELVER
+		1568: {}, // Puddle Terror
+		340:  {}, // Sea Pony
+		162:  {}, // Sinister Squashling
+		1628: {}, // Sister of Temptation
+		200:  {}, // Spring Rabbit
+		211:  {}, // Strand Crawler
+		2088: {}, // Surger
+		1434: {}, // Sun Sproutling
+		1570: {}, // Sunfire Kaliri
+		117:  {}, // Tiny Snowman
+		251:  {}, // Toxic Wasteling
+		118:  {}, // Winter Reindeer
+		120:  {}, // Winter's Little Helper
+		153:  {}, // Wolpertinger
 	}
 
 	for _, petAuction := range auctions[battlepet.PetCageItemId] {
