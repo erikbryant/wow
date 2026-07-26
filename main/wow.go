aaa329dd42ff138ea497660664b19774379e099a
Erik Bryant <erikbryantology@gmail.com>
2026-07-26 11:19:06 -0700

    Simplify file writing

--- main/wow.go
+++ main/wow.go
@@ -4,9 +4,9 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"os"
 
 	"github.com/erikbryant/wow/battlepet"
-	"github.com/erikbryant/wow/common"
 	"github.com/erikbryant/wow/itemcache"
 	"github.com/erikbryant/wow/shopping"
 	"github.com/erikbryant/wow/toy"
@@ -19,6 +19,10 @@ var (
 	realms         = flag.String("realms", "Aegwynn,Agamaggan,Aggramar,Akama,Alexstrasza,Alleria,Altar of Storms,Alterac Mountains,Andorhal,Anub'arak,Argent Dawn,Azgalor,Azjol-Nerub,Azralon,Azuremyst,Baelgun,Barthilas,Blackhand,Blackwing Lair,Bloodhoof,Bloodscalp,Bronzebeard,Caelestrasz,Cairne,Coilfang,Darrowmere,Dath'Remar,Deathwing,Dentarg,Draenor,Dragonblight,Drak'thul,Drakkari,Durotan,Eitrigg,Elune,Eredar,Farstriders,Feathermoon,Frostwolf,Gallywix,Ghostlands,Goldrinn,Greymane,Gundrak,Icecrown,Kilrogg,Kirin Tor,Kul Tiras,Lightninghoof,Llane,Misha,Nazgrel,Nemesis,Quel'Thalas,Ragnaros,Ravencrest,Runetotem,Sisters of Elune,Commodities", "WoW realm(s) to scan")
 	oauthAvailable = flag.Bool("oauth", true, "Is OAuth authentication available?")
 	summarize      = flag.Bool("summarize", true, "Summarize arbitrages?")
+)
+
+const (
+	priceCachePath = "./generated/PriceCache.lua"
 )
 
 // usage prints a usage message and terminates the program with an error
@@ -50,6 +54,9 @@ func main() {
 
 	if *oauthAvailable {
 		// Write the prices file for the WoW 'wowMerchant' addon to consume
-		common.WriteFile("./generated/PriceCache.lua", itemcache.Lua())
+		err := os.WriteFile(priceCachePath, []byte(itemcache.Lua()), 0600)
+		if err != nil {
+			log.Fatal(err)
+		}
 	}
 }
