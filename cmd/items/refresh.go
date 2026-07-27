7f317e94bfa9220790015fb5e950328ea4d817d1
Erik Bryant <erikbryantology@gmail.com>
2026-07-26 18:30:06 -0700

    Fix spelling error

--- cmd/items/refresh.go
+++ cmd/items/refresh.go
@@ -49,7 +49,7 @@ func refreshCache(passphrase string, maxRefresh int) {
 func runRefresh(args []string) {
 	flags := flag.NewFlagSet("refresh", flag.ContinueOnError)
 
-	passphrase := flags.String("passphrase", "", "Passphrase to unlock WOW API client Id/secret")
+	passphrase := flags.String("passphrase", "", "Passphrase to unlock WoW API client Id/secret")
 	maxRefresh := flags.Int("max-refresh", 1000, "Maximum number of items to refresh")
 	itemID := flags.Int64("id", -1, "Item ID to look up")
 
--- cmd/wow/wow.go
+++ cmd/wow/wow.go
@@ -15,7 +15,7 @@ import (
 )
 
 var (
-	passphrase     = flag.String("passphrase", "", "Passphrase to unlock WOW API client Id/secret")
+	passphrase     = flag.String("passphrase", "", "Passphrase to unlock WoW API client Id/secret")
 	realms         = flag.String("realms", "Aegwynn,Agamaggan,Aggramar,Akama,Alexstrasza,Alleria,Altar of Storms,Alterac Mountains,Andorhal,Anub'arak,Argent Dawn,Azgalor,Azjol-Nerub,Azralon,Azuremyst,Baelgun,Barthilas,Blackhand,Blackwing Lair,Bloodhoof,Bloodscalp,Bronzebeard,Caelestrasz,Cairne,Coilfang,Darrowmere,Dath'Remar,Deathwing,Dentarg,Draenor,Dragonblight,Drak'thul,Drakkari,Durotan,Eitrigg,Elune,Eredar,Farstriders,Feathermoon,Frostwolf,Gallywix,Ghostlands,Goldrinn,Greymane,Gundrak,Icecrown,Kilrogg,Kirin Tor,Kul Tiras,Lightninghoof,Llane,Misha,Nazgrel,Nemesis,Quel'Thalas,Ragnaros,Ravencrest,Runetotem,Sisters of Elune,Commodities", "WoW realm(s) to scan")
 	oauthAvailable = flag.Bool("oauth", true, "Is OAuth authentication available?")
 	summarize      = flag.Bool("summarize", true, "Summarize arbitrages?")
