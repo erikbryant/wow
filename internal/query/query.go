a7657f1906f8fdb6ca8a174f4293937626a5f938
Erik Bryant <erikbryantology@gmail.com>
2026-07-28 10:44:03 -0700

    Consolidate common code fragments

--- internal/query/query.go
+++ internal/query/query.go
@@ -5,8 +5,7 @@ import "github.com/erikbryant/wow/internal/wowitem"
 // Predicate is a function that determines whether an item matches a query.
 type Predicate func(wowitem.Item) bool
 
-// Find returns all items matching every supplied predicate.
-func Find(items []wowitem.Item, predicates ...Predicate) []wowitem.Item {
+func finder(items []wowitem.Item, predicates ...Predicate) []wowitem.Item {
 	results := make([]wowitem.Item, 0)
 
 	for _, item := range items {
@@ -27,44 +26,17 @@ func Find(items []wowitem.Item, predicates ...Predicate) []wowitem.Item {
 	return results
 }
 
-// Any returns true if at least one item matches all supplied predicates.
-func Any(items []wowitem.Item, predicates ...Predicate) bool {
-	for _, item := range items {
-		match := true
-
-		for _, predicate := range predicates {
-			if !predicate(item) {
-				match = false
-				break
-			}
-		}
-
-		if match {
-			return true
-		}
-	}
-
-	return false
+// Find returns all items matching every supplied predicate.
+func Find(items []wowitem.Item, predicates ...Predicate) []wowitem.Item {
+	return finder(items, predicates...)
 }
 
 // Count returns the number of items matching all supplied predicates.
 func Count(items []wowitem.Item, predicates ...Predicate) int {
-	count := 0
+	return len(finder(items, predicates...))
+}
 
-	for _, item := range items {
-		match := true
-
-		for _, predicate := range predicates {
-			if !predicate(item) {
-				match = false
-				break
-			}
-		}
-
-		if match {
-			count++
-		}
-	}
-
-	return count
+// Any returns true if at least one item matches all supplied predicates.
+func Any(items []wowitem.Item, predicates ...Predicate) bool {
+	return Count(items, predicates...) > 0
 }
