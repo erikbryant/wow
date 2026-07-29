d95140cf27c5fb2d6064822daf8bee5b484bc9d4
Erik Bryant <erikbryantology@gmail.com>
2026-07-29 09:24:51 -0700

    Remove dead code

--- internal/query/query.go
+++ internal/query/query.go
@@ -5,7 +5,8 @@ import "github.com/erikbryant/wow/internal/wowitem"
 // Predicate is a function that determines whether an item matches a query.
 type Predicate func(wowitem.Item) bool
 
-func finder(items []wowitem.Item, predicates ...Predicate) []wowitem.Item {
+// Find returns all items matching every supplied predicate.
+func Find(items []wowitem.Item, predicates ...Predicate) []wowitem.Item {
 	results := make([]wowitem.Item, 0)
 
 	for _, item := range items {
@@ -24,23 +25,4 @@ func finder(items []wowitem.Item, predicates ...Predicate) []wowitem.Item {
 	}
 
 	return results
-}
-
-// Find returns all items matching every supplied predicate.
-func Find(items []wowitem.Item, predicates ...Predicate) []wowitem.Item {
-	return finder(items, predicates...)
-}
-
-// Count returns the number of items matching all supplied predicates.
-func Count(items []wowitem.Item, predicates ...Predicate) int {
-	// This could be made memory efficient by not allocating the slice.
-	// But that leads to complexity. And this function isn't even used.
-	return len(finder(items, predicates...))
-}
-
-// Any returns true if at least one item matches all supplied predicates.
-func Any(items []wowitem.Item, predicates ...Predicate) bool {
-	// This could be made faster by short-circuiting when the first is found.
-	// But that leads to complexity. And this function isn't even used.
-	return Count(items, predicates...) > 0
 }
