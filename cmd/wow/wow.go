ad70d44c0fdef27b101d70c406a94677b09b2880
Erik Bryant <erikbryantology@gmail.com>
2026-08-02 22:47:12 -0700

    Consistent code ordering: type, const, var, funcs

--- cmd/wow/wow.go
+++ cmd/wow/wow.go
@@ -10,14 +10,14 @@ import (
 	"github.com/erikbryant/wow/internal/wowitem"
 )
 
+const (
+	priceCachePath = "./exports/PriceCache.lua"
+)
+
 var (
 	passphrase = flag.String("passphrase", "", "Passphrase to unlock WoW API client ID/secret")
 	realms     = flag.String("realms", "Aegwynn,Agamaggan,Aggramar,Akama,Alexstrasza,Alleria,Altar of Storms,Alterac Mountains,Andorhal,Anub'arak,Argent Dawn,Azgalor,Azjol-Nerub,Azralon,Azuremyst,Baelgun,Barthilas,Blackhand,Blackwing Lair,Bloodhoof,Bloodscalp,Bronzebeard,Caelestrasz,Cairne,Coilfang,Darrowmere,Dath'Remar,Deathwing,Dentarg,Draenor,Dragonblight,Drak'thul,Drakkari,Durotan,Eitrigg,Elune,Eredar,Farstriders,Feathermoon,Frostwolf,Gallywix,Ghostlands,Goldrinn,Greymane,Gundrak,Icecrown,Kilrogg,Kirin Tor,Kul Tiras,Lightninghoof,Llane,Misha,Nazgrel,Nemesis,Quel'Thalas,Ragnaros,Ravencrest,Runetotem,Sisters of Elune,Commodities", "WoW realm(s) to scan")
 	summarize  = flag.Bool("summarize", true, "Summarize arbitrages?")
-)
-
-const (
-	priceCachePath = "./exports/PriceCache.lua"
 )
 
 // usage prints a usage message and terminates the program with an error
