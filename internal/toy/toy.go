54e49341e2ce34e7f22f2c45b26a24c6c69a7721
Erik Bryant <erikbryantology@gmail.com>
2026-08-14 09:38:32 -0700

    Fix initialization problems, make Owned() efficient

--- internal/toy/toy.go
+++ internal/toy/toy.go
@@ -2,7 +2,6 @@ package toy
 
 import (
 	"fmt"
-	"sync"
 
 	"github.com/erikbryant/web"
 	"github.com/erikbryant/wow/internal/wowapi"
@@ -10,18 +9,18 @@ import (
 )
 
 type Toy struct {
-	names map[int64]string
+	// Toy IDs. These are not the same as item IDs.
+	names map[string]int64
 	owned map[int64]bool
 }
 
 var (
-	once = sync.Once{}
 	toys *Toy
 )
 
-// getNames returns a map of all toy names by ID
-func getNames() (map[int64]string, error) {
-	toyNames := map[int64]string{}
+// getNames returns a map of all toy names by toy ID
+func getNames() (map[string]int64, error) {
+	toyNames := map[string]int64{}
 
 	allToys, err := wowapi.Toys()
 	if err != nil {
@@ -31,13 +30,13 @@ func getNames() (map[int64]string, error) {
 	for _, toyRaw := range allToys {
 		toy := toyRaw.(map[string]any)
 		id := web.ToInt64(toy["id"])
-		toyNames[id] = toy["name"].(string)
+		toyNames[toy["name"].(string)] = id
 	}
 
 	return toyNames, nil
 }
 
-// getOwned returns the toys I own
+// getOwned returns the toys I own by toy ID
 func getOwned() (map[int64]bool, error) {
 	myToys := map[int64]bool{}
 
@@ -55,38 +54,42 @@ func getOwned() (map[int64]bool, error) {
 	return myToys, nil
 }
 
-func get() error {
+func get() (*Toy, error) {
 	var t Toy
 	var err error
 
 	t.names, err = getNames()
 	if err != nil {
-		return err
+		return nil, err
 	}
 
 	t.owned, err = getOwned()
 	if err != nil {
-		return err
+		return nil, err
 	}
 
-	toys = &t
-
-	return nil
+	return &t, nil
 }
 
 func New() (*Toy, error) {
+	if toys != nil {
+		return toys, nil
+	}
+
 	var err error
-	once.Do(func() {
-		err = get()
-	})
+	toys, err = get()
+	if err != nil {
+		return nil, err
+	}
+
 	return toys, err
 }
 
+// Owned returns true if the item name is a toy and we own it
 func (t *Toy) Owned(i wowitem.Item) bool {
-	for toyID, name := range t.names {
-		if i.Name() == name {
-			return t.owned[toyID]
-		}
+	toyID, ok := t.names[i.Name()]
+	if !ok {
+		return false
 	}
-	return false
+	return t.owned[toyID]
 }
