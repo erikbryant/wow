6c9efe32abfb27996047fa50457f37119a8e14e5
Erik Bryant <erikbryantology@gmail.com>
2026-08-17 20:55:26 -0700

    Add more synthetic items, allow calls to be chained

--- cmd/wowctl/synthetic.go
+++ cmd/wowctl/synthetic.go
@@ -12,17 +12,63 @@ import (
 	"github.com/erikbryant/wow/internal/wowitem"
 )
 
+func newWidget(id int64, name string) wowitem.Item {
+	item := syntheticitem.New(id).
+		SetName(name).
+		SetLevel(1).
+		SetItemClassName("Miscellaneous")
+
+	return wowitem.NewItem(item.Map())
+}
+
 // synthetics returns the synthetic items we have created.
 func synthetics() []wowitem.Item {
 	var item *syntheticitem.Item
 	items := []wowitem.Item{}
 
-	item = syntheticitem.New(226002)
-	item.SetName("Expensive-Looking Find")
-	item.SetPreviewPrice(common.Coppers(200, 0, 0))
-	item.SetStackable(false)
-	item.SetLevel(23)
-	item.SetItemClassName("Miscellaneous")
+	// Items that DO NOT have vendor prices. Just create a lightweight entry.
+
+	items = append(items, newWidget(123865, "Relic of Ursol"))
+	items = append(items, newWidget(123865, "Relic of Ursol"))
+	items = append(items, newWidget(123868, "Relic of Shakama"))
+	items = append(items, newWidget(123869, "Relic of Elune"))
+	items = append(items, newWidget(147455, "Water Stone"))
+	items = append(items, newWidget(203932, "Sentient Book"))
+	items = append(items, newWidget(217959, "Incomplete Painting"))
+	items = append(items, newWidget(225218, "Echoing Fragment: Hallowfall"))
+	items = append(items, newWidget(225219, "Echoing Fragment: The Ringing Deeps"))
+	items = append(items, newWidget(225236, "Echoing Fragment: Isle of Dorn"))
+	items = append(items, newWidget(225237, "Echoing Fragment: Azj-Kahet"))
+	items = append(items, newWidget(268944, "Souvenir Halazzi Idol"))
+	items = append(items, newWidget(268945, "Souvenir Nalorakk Mask"))
+	items = append(items, newWidget(268946, "Souvenir Jan'alai Key Chain"))
+	items = append(items, newWidget(268947, "Souvenir Akil'zon Shine Paper Weight"))
+	items = append(items, newWidget(268948, "Fine Antique Silvermoon Drapes"))
+	items = append(items, newWidget(268949, "Single Earthen Salt Shaker"))
+	items = append(items, newWidget(275670, "Bill of Lading"))
+
+	// Items that DO have vendor prices. These are much more interesting.
+	// They might actually become arbitrage plays.
+
+	item = syntheticitem.New(217958).
+		SetName("Used Socks").
+		SetLevel(1).
+		SetPreviewPrice(common.Coppers(0, 0, 1)).
+		SetItemClassName("Miscellaneous")
+	items = append(items, wowitem.NewItem(item.Map()))
+
+	item = syntheticitem.New(226002).
+		SetName("Expensive-Looking Find").
+		SetLevel(23).
+		SetPreviewPrice(common.Coppers(200, 0, 0)).
+		SetItemClassName("Miscellaneous")
+	items = append(items, wowitem.NewItem(item.Map()))
+
+	item = syntheticitem.New(226004).
+		SetName("Olden Text").
+		SetLevel(23).
+		SetPreviewPrice(common.Coppers(200, 0, 0)).
+		SetItemClassName("Miscellaneous")
 	items = append(items, wowitem.NewItem(item.Map()))
 
 	return items
@@ -57,7 +103,7 @@ func syntheticPopulate(paths *path.Paths) error {
 		return fmt.Errorf("failed to save item persistence: %w", err)
 	}
 
-	fmt.Fprintf(os.Stdout, "Populated %d items:\n\n", len(s))
+	fmt.Fprintf(os.Stdout, "\nPopulated %d items:\n\n", len(s))
 	err = syntheticList(paths)
 	if err != nil {
 		return err
--- internal/syntheticitem/syntheticitem.go
+++ internal/syntheticitem/syntheticitem.go
@@ -10,11 +10,14 @@ type Item struct {
 }
 
 func New(id int64) *Item {
-	return &Item{
-		data: map[string]any{
-			"id": json.Number(strconv.FormatInt(id, 10)),
-		},
+	i := Item{
+		data: map[string]any{},
 	}
+
+	i.SetID(id)
+	i.SetStackable(false)
+
+	return &i
 }
 
 func (i *Item) Map() map[string]any {
@@ -25,64 +28,82 @@ func (i *Item) JSON() ([]byte, error) {
 	return json.MarshalIndent(i.data, "", "  ")
 }
 
-func (i *Item) SetBinding(binding string) {
+func (i *Item) SetID(id int64) *Item {
+	i.set([]string{"id"}, json.Number(strconv.FormatInt(id, 10)))
+	return i
+}
+
+func (i *Item) SetBinding(binding string) *Item {
 	i.set([]string{"preview_item", "binding", "type"}, binding)
+	return i
 }
 
-func (i *Item) SetInventoryType(inventoryType string) {
+func (i *Item) SetInventoryType(inventoryType string) *Item {
 	i.set([]string{"inventory_type", "type"}, inventoryType)
+	return i
 }
 
-func (i *Item) SetLevel(level int64) {
+func (i *Item) SetLevel(level int64) *Item {
 	i.set([]string{"level"}, json.Number(strconv.FormatInt(level, 10)))
+	return i
 }
 
-func (i *Item) SetItemSubclassName(name string) {
+func (i *Item) SetItemSubclassName(name string) *Item {
 	i.set([]string{"item_subclass", "name"}, name)
+	return i
 }
 
-func (i *Item) SetItemClassName(name string) {
+func (i *Item) SetItemClassName(name string) *Item {
 	i.set([]string{"item_class", "name"}, name)
+	return i
 }
 
-func (i *Item) SetStackable(stackable bool) {
+func (i *Item) SetStackable(stackable bool) *Item {
 	i.set([]string{"is_stackable"}, stackable)
+	return i
 }
 
-func (i *Item) SetRelicType(relicType string) {
+func (i *Item) SetRelicType(relicType string) *Item {
 	i.set([]string{"preview_item", "gem_properties", "relic_type"}, relicType)
+	return i
 }
 
-func (i *Item) SetPreviewPrice(value int64) {
+func (i *Item) SetPreviewPrice(value int64) *Item {
 	i.set([]string{"preview_item", "sell_price", "value"}, json.Number(strconv.FormatInt(value, 10)))
+	return i
 }
 
-func (i *Item) SetRequiredSkill(displayString string) {
+func (i *Item) SetRequiredSkill(displayString string) *Item {
 	i.set([]string{
 		"preview_item",
 		"requirements",
 		"skill",
 		"display_string",
 	}, displayString)
+	return i
 }
 
-func (i *Item) SetQuality(name string) {
+func (i *Item) SetQuality(name string) *Item {
 	i.set([]string{"preview_item", "quality", "name"}, name)
+	return i
 }
 
-func (i *Item) SetName(name string) {
+func (i *Item) SetName(name string) *Item {
 	i.set([]string{"name"}, name)
+	return i
 }
 
-func (i *Item) SetToy(toy bool) {
+func (i *Item) SetToy(toy bool) *Item {
 	i.set([]string{"preview_item", "toy"}, toy)
+	return i
 }
 
-func (i *Item) SetAppearances(appearances []any) {
+func (i *Item) SetAppearances(appearances []any) *Item {
 	i.set([]string{"appearances"}, appearances)
+	return i
 }
 
-func (i *Item) set(keys []string, value any) {
+func (i *Item) set(keys []string, value any) *Item {
 	object := i.data
 
 	for _, key := range keys[:len(keys)-1] {
@@ -96,4 +117,6 @@ func (i *Item) set(keys []string, value any) {
 	}
 
 	object[keys[len(keys)-1]] = value
+
+	return i
 }
